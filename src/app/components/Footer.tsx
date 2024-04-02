da5397efca13c42f0baa8878683173da3d1bd699
umer farooq <urfq_farooq@hotmail.com>
2024-04-02 08:00:39 +0500

    add Logo and styles header and footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,24 +1,28 @@
 import { createClient } from "@/prismicio"
 import { PrismicNextLink } from "@prismicio/next"
 import Link from "next/link"
+import Bounded from "./Bounded"
+import Logo from "./Logo"
 
 
 export default async function Footer () {
 	const client = createClient()
 	const settings = await client.getSingle('settings')
 	return (
-		<footer>
+		<Bounded as= "footer">
+			<div className="flex gap-4 items-center justify-between sm:flex-row flex-col">
 			<Link href="/">
-				{settings.data.site_title}
+				<Logo />
 			</Link>
-			<p>©{new Date().getFullYear()} {settings.data.site_title}</p>
-			<ul>
+			<p className="text-xs">©{new Date().getFullYear()} {settings.data.site_title}</p>
+			<ul className="flex">
 			{settings.data.navigation.map(({link, label})=>(
 				<li key={label}>
-					<PrismicNextLink field={link}>{label}</PrismicNextLink>
+					<PrismicNextLink className="p-3" field={link}>{label}</PrismicNextLink>
 				</li>
 			))}
 		</ul>
-		</footer>
+		</div>
+		</Bounded>
 	)
 }
